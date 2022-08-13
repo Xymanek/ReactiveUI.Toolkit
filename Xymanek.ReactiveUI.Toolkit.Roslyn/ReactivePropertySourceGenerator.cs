d5f3826240c49c85c4e6d4a91f790d55f6ecb31f
Xymanek <xymanek@outlook.com>
2022-08-13 11:20:16 +0300

    Basic sgen setup

---
+++ Xymanek.ReactiveUI.Toolkit.Roslyn/ReactivePropertySourceGenerator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.CodeAnalysis;
+
+namespace Xymanek.ReactiveUI.Toolkit.Roslyn;
+
+[Generator]
+public class ReactivePropertySourceGenerator : IIncrementalGenerator
+{
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        // throw new Exception("oops");
+    }
+}
