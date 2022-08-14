df2d8f88e468cb08237f0223f11b713f85476e18
Xymanek <xymanek@outlook.com>
2022-08-14 12:06:53 +0300

    Console testbed app

---
+++ Xymanek.ReactiveUI.Toolkit.Demo.Console/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ReactiveUI;
+using Xymanek.ReactiveUI.Toolkit.Demo.ViewModels;
+
+Console.WriteLine("Hello, World!");
+
+UserInfoViewModel userInfoViewModel = new();
+
+userInfoViewModel.Username = "user0";
+userInfoViewModel.Username = "user1";
+
+// Too lazy to dispose
+userInfoViewModel.WhenAnyValue(u => u.Username)
+    .Subscribe(username => Console.WriteLine("WhenAnyValue(u => u.Username): " + username));
+
+userInfoViewModel.Username = "user2";
+userInfoViewModel.Username = "user3";
+
+Console.WriteLine("Final username: " + userInfoViewModel.Username);
